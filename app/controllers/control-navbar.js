9ad7088de02cfce612449184ee8f8b6d15fc6998
Hiatt Collins <hiatt.collins.github@gmail.com>
2017-12-12 16:52:35 -0600

    basic file structure created for testing api

---
+++ app/controllers/control-navbar.js
@@ -0,0 +1,34 @@
+"use strict";
+
+app.controller("navbarCtrl", function($rootScope, $scope, $window, $location, $q, $http){
+
+
+
+    console.log("navbarCtrl activated");
+
+    // $scope.loginClicked = function () {
+    //     userFactory.doLogIn();
+    // };
+
+    // $scope.logoutClicked = function () {
+    //     userFactory.doLogout();
+    //     $rootScope.showLogoutButton = false;
+    // };
+
+    // firebase.auth().onAuthStateChanged(function(user) {
+    //     console.log("control-navbar firebase user:", user);
+    // });
+
+    // $scope.statCheck = function () {
+    //     console.log("statCheck activated");
+    //     userFactory.factoryCheckStatus();
+    // };
+
+    // $scope.firebaseCheck = function () {
+    //     let firebaseUserinfo = userFactory.getFirebaseUser();
+    //     console.log("firebaseUserinfo:", firebaseUserinfo);
+    //     console.log("firebaseUserinfo.uid:", firebaseUserinfo.uid);
+    // };
+
+
+});
---
+++ app/controllers/control-query.js
@@ -0,0 +1,41 @@
+"use strict";
+
+app.controller("queryCtrl", function($scope, $window, $location, $q, $http, queryFactory){
+
+  console.log("control-query.js");
+
+  $scope.queries = [];
+
+    const showAllQueries = function(){
+      queryFactory.getAllQueries()
+      .then((queries) => {
+        console.log("showAllQueries from promise", queries);
+        // $scope.queries = queries;
+      });
+    };
+
+
+    // let query_to_submit = {
+    //   "text input": {
+    //     "query_id": 1,
+    //     "input_text": "Methinks that what they call my shadow here on earth is my true substance. Methinks that in looking at things spiritual, we are too much like oysters observing the sun through the water, and thinking that thick water the thinnest of air. Methinks my body is but the lees of my better being. In fact take my body who will, take it I say, it is not me. And therefore three cheers for Nantucket; and come a stove boat and stove body when they will, for stave my soul, Jove himself cannot."
+    //   }
+    // };
+
+    let query_to_submit = {
+        "query_id": 1,
+        "input_text": "Methinks that what they call my shadow here on earth is my true substance. Methinks that in looking at things spiritual, we are too much like oysters observing the sun through the water, and thinking that thick water the thinnest of air. Methinks my body is but the lees of my better being. In fact take my body who will, take it I say, it is not me. And therefore three cheers for Nantucket; and come a stove boat and stove body when they will, for stave my soul, Jove himself cannot."
+    };
+
+    const newQuery = function(query){
+      queryFactory.submitQuery(query)
+      .then((result) => {
+        console.log("newQuery result from promise", result);
+        // $scope.queries = queries;
+      });
+    };
+
+    // showAllQueries();
+    newQuery(query_to_submit);
+
+});
