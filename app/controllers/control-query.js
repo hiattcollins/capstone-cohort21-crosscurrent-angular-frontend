aaa4be5821fbac6f3ed11e2058672ee346117d18
Hiatt Collins <hiatt.collins.github@gmail.com>
2017-12-15 04:34:28 -0600

    query function work

--- app/controllers/control-query.js
+++ app/controllers/control-query.js
@@ -15,17 +15,37 @@ app.controller("queryCtrl", function($rootScope, $scope, $window, $location, $q,
     };
 
 
-    // let query_to_submit = {
-    //   "text input": {
-    //     "query_id": 1,
-    //     "input_text": "Methinks that what they call my shadow here on earth is my true substance. Methinks that in looking at things spiritual, we are too much like oysters observing the sun through the water, and thinking that thick water the thinnest of air. Methinks my body is but the lees of my better being. In fact take my body who will, take it I say, it is not me. And therefore three cheers for Nantucket; and come a stove boat and stove body when they will, for stave my soul, Jove himself cannot."
-    //   }
-    // };
 
     let query_to_submit = {
         "query_id": 1,
         "input_text": "Methinks that what they call my shadow here on earth is my true substance. Methinks that in looking at things spiritual, we are too much like oysters observing the sun through the water, and thinking that thick water the thinnest of air. Methinks my body is but the lees of my better being. In fact take my body who will, take it I say, it is not me. And therefore three cheers for Nantucket; and come a stove boat and stove body when they will, for stave my soul, Jove himself cannot."
     };
+
+    $scope.credentials = {
+      email: null,
+      password: null
+    };
+
+    // $scope.loginClicked = function () {
+    //   console.log("loginClicked() activated");
+    //   userFactory.doLogIn({
+    //     email: $scope.credentials.email,
+    //     password: $scope.credentials.password
+    //   })
+    //   .then((userAuth) => {
+    //     console.log("information from auth:", userAuth);
+    //     $rootScope.auth_token = userAuth.auth_token;
+    //     $rootScope.user_id = userAuth.user_id;
+    //     $window.alert("Login successful!");
+    //     $rootScope.isLoggedIn = true;
+    //     $location.path("/query");
+    //   }, (error) => {
+    //     console.log("error logging in", error);
+    //     $window.alert("Invalid login info!");
+    //     $scope.credentials.email = null;
+    //     $scope.credentials.password = null;
+    //   });
+    // };
 
     const newQuery = function(query){
       queryFactory.submitQuery(query)
