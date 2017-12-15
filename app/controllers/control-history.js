174420e070230934dccf873e9db97e475a38a4ef
Hiatt Collins <hiatt.collins.github@gmail.com>
2017-12-15 13:43:42 -0600

    work on history partial and function, and complete core functionality

--- app/controllers/control-history.js
+++ app/controllers/control-history.js
@@ -4,4 +4,20 @@ app.controller("historyCtrl", function($rootScope, $scope, $window, $location, $
 
   console.log("control-history.js");
 
+    // $scope.newQuery = function () {
+    //   console.log("newQuery() activated");
+    //   queryFactory.submitQuery({query: {
+    //     user_id: $rootScope.user_id,
+    //     input_text: $scope.new_query.input_text
+    //     }
+    //   }, $rootScope.auth_token)
+    //   .then((resultingSongs) => {
+    //     console.log("resultingSongs:", resultingSongs);
+    //     $scope.songsToShow = resultingSongs
+    //   }, (error) => {
+    //     console.log("error logging in", error);
+    //     $window.alert("There was an error with the query.");
+    //   });
+    // };
+
 });
