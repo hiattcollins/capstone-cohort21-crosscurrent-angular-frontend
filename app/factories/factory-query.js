e5f353a6a5bfefa14b36276f5fa45a571c392fb0
Hiatt Collins <hiatt.collins.github@gmail.com>
2018-01-06 12:00:17 -0600

    completed single history delete functionality, added route resolve functionality to check for login

--- app/factories/factory-query.js
+++ app/factories/factory-query.js
@@ -5,27 +5,26 @@ app.factory("queryFactory", function ($q, $http, $window) {
 
 
 
-    const getAllQueries = function(){
-        let queries = [];
-        console.log("url is", `http://localhost:3000/text_inputs`);
-        return $q((resolve, reject) => {
-            $http.get(`http://localhost:3000/text_inputs`)
-            .then((queryObject) => {
-                // let queryCollection = queryObject.data;
-                // console.log("itemCollection", itemCollection);
-                // Object.keys(itemCollection).forEach((key) => {
-                //     itemCollection[key].id = key;
-                //     tasks.push(itemCollection[key]);
-                // });
-                resolve(queryObject);
-            })
-            .catch((error) => {
-                reject(error);
-            });
-        });
-    };
+    // const getAllQueries = function(){
+    //     let queries = [];
+    //     console.log("url is", `http://localhost:3000/text_inputs`);
+    //     return $q((resolve, reject) => {
+    //         $http.get(`http://localhost:3000/text_inputs`)
+    //         .then((queryObject) => {
+    //             // let queryCollection = queryObject.data;
+    //             // console.log("itemCollection", itemCollection);
+    //             // Object.keys(itemCollection).forEach((key) => {
+    //             //     itemCollection[key].id = key;
+    //             //     tasks.push(itemCollection[key]);
+    //             // });
+    //             resolve(queryObject);
+    //         })
+    //         .catch((error) => {
+    //             reject(error);
+    //         });
+    //     });
+    // };
 
-// headers: { 'Content-Type': 'application/json', 'Authorization': `${token}`}
 
     const submitQuery = function(queryObj, token) {
         let queryToSubmit = JSON.stringify(queryObj);
@@ -46,6 +45,45 @@ app.factory("queryFactory", function ($q, $http, $window) {
         });
     };
 
+    const getQueryHistory = function(userObj, token){
+        let userToSubmit = JSON.stringify(userObj);
+        console.log("userObj:", userObj, "token:", token);
+        return $q((resolve, reject) => {
+            $http.post(`http://localhost:3000/get_by_user`, userToSubmit, {
+              headers: {'Authorization': token}
+            })
+            .then((historyResults) => {
+                console.log("historyResults:", historyResults);
+                let historyArray = historyResults.data;
+                resolve(historyArray);
+            })
+            .catch((error) => {
+                console.log("getQueryHistory error");
+                reject(error);
+            });
+        });
+    };
+
+    const deleteOneQueryFromDB = function(queryId, token) {
+      console.log("queryId:", queryId, "token:", token);
+      return $q((resolve, reject) => {
+        $http.delete(`http://localhost:3000/queries/${queryId}`, {
+          headers: {'Authorization': token}
+        })
+        .then((deleteOneResults) => {
+          resolve(deleteOneResults);
+        })
+        .catch((error) => {
+          console.log("deleteOneQueryFromDB error");
+          reject(error);
+        });
+      });
+    };
+
+// headers: { 'Content-Type': 'application/json', 'Authorization': `${token}`}
+
+
+
     // const submitQuery = function(obj){
     //     let newObj = JSON.stringify(obj);
     //     return $q((resolve, reject) => {`${FBCreds.databaseURL}/items.json`, newObj)
@@ -60,6 +98,6 @@ app.factory("queryFactory", function ($q, $http, $window) {
     // };
 
 
-    return { getAllQueries, submitQuery };
+    return { submitQuery, getQueryHistory, deleteOneQueryFromDB }; //getAllQueries,
 
 });
